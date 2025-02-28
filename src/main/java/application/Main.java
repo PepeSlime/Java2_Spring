6192349198652f81bb5f803aa1e94e7c74bd2782
PepeSlime <128192489+PepeSlime@users.noreply.github.com>
2025-02-28 19:44:18 +0000

    .

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,8 +1,10 @@
 package application;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        SpringApplication.run(Main.class, args);
     }
 }
 
